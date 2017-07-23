6398147d2b8fe2e83e70557e5f2631f884f5eb28
kmishra9 <kmishra9@berkeley.edu>
2017-07-23 14:17:28 -0700

    Updates to update script

---
+++ C9_Update.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "Updating your Cloud 9 Workspace"
+
+#Known issue: resets OK tests too
+cd ~/workspace/Assignments/a0
+cp ./lab00.py ./temp_copy
+cd ~/workspace/Assignments/a1
+cp ./lab01.py ./temp_copy
+cd ~/workspace/Assignments/a2
+cp ./hw01.py ./temp_copy
+
+cd ~/workspace
+git reset --hard
+git pull
+
+cd ~/workspace/Assignments/a0
+mv ./temp_copy ./lab00.py
+cd ~/workspace/Assignments/a1
+mv ./temp_copy ./lab01.py
+cd ~/workspace/Assignments/a2
+mv ./temp_copy ./hw01.py
+
+echo "Updating Project 0 - RPS"
+cd ~/workspace/RPS
+cp ./starter_RPS.py ./temp_copy
+git reset --hard
+git pull
+mv ./temp_copy ./starter_RPS.py
+
+echo "Updating Project 1 - 2048"
+cd ~/workspace/2048-Starter
+cp ./starter_2048.py ./temp_copy
+git reset --hard
+git pull
+mv ./temp_copy ./starter_2048.py
+
+echo "Updating Project 2 - RPS.ai"
+cd ~/workspace/AI-Extensions
+cp ./agents.py ./temp_copy
+git reset --hard
+git pull
+mv ./temp_copy ./agents.py
+
+echo "Updating Project 3 - Hog"
+cd ~/workspace/Hog
+git pull
+
+echo "Resetting all permissions back to users"
+sudo chown -R ubuntu *
